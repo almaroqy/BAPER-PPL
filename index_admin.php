775731d14e3cafe2122e66b7a0f26b4bff600483
Irvan Muhandis <irvanmuhandis@gmail.com>
2021-12-08 02:29:52 +0700

    nambah sign up

--- index_admin.php
+++ index_admin.php
@@ -1,117 +1,124 @@
+<?php
+include('./balikkelogin.php');
 
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- FONT AWESOME -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300&family=Roboto+Mono:wght@100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300&family=Roboto+Mono:wght@100&display=swap" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="Front end/style/index_admin.css" />
     <title>Baper!</title>
 </head>
+
 <body>
-<?php
+    <?php
     include('nav_admin.php');
-?>
-<main>
-    <div class="container" style="padding-top: 13%; ">
-        <div class="row">
-            <div class="col">
-                <div class="src-tmbh" style="padding-left: 10%;">
-                    <input type="search" name="search" placeholder="Cari Data Buku">
+    ?>
+    <main>
+        <div class="container" style="padding-top: 13%; ">
+            <div class="row">
+                <div class="col">
+                    <div class="src-tmbh" style="padding-left: 10%;">
+                        <input type="search" name="search" placeholder="Cari Data Buku">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container color-white" style="padding-top: 1%; ">
-        <div class="row">
-            <div class="col">
-                <div class="tmb-book" style="padding-left: 80% ;">
-                    <a href="tambah_buku.php" class="btn btn-success btn-xxl" >Tambah Buku</a>
+        <div class="container color-white" style="padding-top: 1%; ">
+            <div class="row">
+                <div class="col">
+                    <div class="tmb-book" style="padding-left: 80% ;">
+                        <a href="tambah_buku.php" class="btn btn-success btn-xxl">Tambah Buku</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container text-center" style="padding-top: 3%;">
-        <div class="row" style="padding-left: 5%; padding-right: 5%; width: 100%;">
-            <div class="col-md-12">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Foto</th>
-                            <th>Judul</th>
-                            <th>Penulis</th>
-                            <th>Penerbit</th>
-                            <th>Stok</th>
-                            <th>Stok Tersedia</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>
-                                <img src="../baper-1/Front end/image/BukuDongeng.png" alt="" style="height: 90px;">
-                            </td>
-                            <td>Pop Ice 1</td>
-                            <td>Fahmi</td>
-                            <td>Fahmi Corp</td>
-                            <td>12</td>
-                            <td>11</td>
-                            <td> 
-                                <div class="actn">
-                                    <a href="" class="btn btn-info btn-md">Edit</a>
-                                <a href="" class="btn btn-danger btn-md" >Hapus</a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>
-                                <img src="../baper-1/Front end/image/BukuIlmu.png" alt="" style="height: 90px;">
-                            </td>
-                            <td>Pop Ice 1</td>
-                            <td>Fahmi</td>
-                            <td>Fahmi Corp</td>
-                            <td>12</td>
-                            <td>11</td>
-                            <td> 
-                                <div class="actn">
-                                    <a href="" class="btn btn-info btn-md">Edit</a>
-                                <a href="" class="btn btn-danger btn-md" >Hapus</a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>
-                                <img src="../baper-1/Front end/image/BukuHoror.png" alt="" style="height: 90px;">
-                            </td>
-                            <td>Pop Ice 1</td>
-                            <td>Fahmi</td>
-                            <td>Fahmi Corp</td>
-                            <td>12</td>
-                            <td>11</td>
-                            <td> 
-                                <div class="actn">
-                                    <a href="" class="btn btn-info btn-md">Edit</a>
-                                <a href="" class="btn btn-danger btn-md" >Hapus</a>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div class="container text-center" style="padding-top: 3%;">
+            <div class="row" style="padding-left: 5%; padding-right: 5%; width: 100%;">
+                <div class="col-md-12">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Foto</th>
+                                <th>Judul</th>
+                                <th>Penulis</th>
+                                <th>Penerbit</th>
+                                <th>Stok</th>
+                                <th>Stok Tersedia</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>
+                                    <img src="../baper-1/Front end/image/BukuDongeng.png" alt="" style="height: 90px;">
+                                </td>
+                                <td>Pop Ice 1</td>
+                                <td>Fahmi</td>
+                                <td>Fahmi Corp</td>
+                                <td>12</td>
+                                <td>11</td>
+                                <td>
+                                    <div class="actn">
+                                        <a href="" class="btn btn-info btn-md">Edit</a>
+                                        <a href="" class="btn btn-danger btn-md">Hapus</a>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>
+                                    <img src="../baper-1/Front end/image/BukuIlmu.png" alt="" style="height: 90px;">
+                                </td>
+                                <td>Pop Ice 1</td>
+                                <td>Fahmi</td>
+                                <td>Fahmi Corp</td>
+                                <td>12</td>
+                                <td>11</td>
+                                <td>
+                                    <div class="actn">
+                                        <a href="" class="btn btn-info btn-md">Edit</a>
+                                        <a href="" class="btn btn-danger btn-md">Hapus</a>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>
+                                    <img src="../baper-1/Front end/image/BukuHoror.png" alt="" style="height: 90px;">
+                                </td>
+                                <td>Pop Ice 1</td>
+                                <td>Fahmi</td>
+                                <td>Fahmi Corp</td>
+                                <td>12</td>
+                                <td>11</td>
+                                <td>
+                                    <div class="actn">
+                                        <a href="" class="btn btn-info btn-md">Edit</a>
+                                        <a href="" class="btn btn-danger btn-md">Hapus</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
     <footer class="Footer-menu mt-5 border-top" id="Section-footer">
         <div class="container" style="background-color: #2c2c2c;">
@@ -121,8 +128,7 @@
                         <div class="row">
                             <div class="col-12 col-md-6 col-lg-3 mb-3">
                                 <ul class="list-unstyled footer-link-list">
-                                    <li><img class="mb-2 imglogobrand" src="Front end/image/Group 103.png"
-                                            alt="imgLogo" /></li>
+                                    <li><img class="mb-2 imglogobrand" src="Front end/image/Group 103.png" alt="imgLogo" /></li>
                                 </ul>
                             </div>
                             <div class="col-12 col-md-6 col-lg-3 mb-3 gradi">
@@ -167,9 +173,8 @@
         </div>
     </footer>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/5b9f1690ea.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,13 +1,39 @@
 <?php
+include('./db_login.php');
+
 if (isset($_POST['daftar'])) {
     $nama = $_POST['nama'];
     $pwd = $_POST['pwd1'];
     $email = $_POST['email'];
     $tlp = $_POST['tlp'];
-    $tmpt = $_POST['tmpt'];
     $tgl = $_POST['tgl'];
     $alamat = $_POST['alamat'];
-    echo $alamat;
+    $gambar = $_FILES['gambar']['name'];
+    $path = $_FILES['gambar']['tmp_name'];
+    move_uploaded_file($path, './Front end/image/' . $gambar);
+    $query = 'select * from user where email ="' . $email . '"';
+    $aw = $db->query($query);
+    if ($aw) {
+        if ($aw->num_rows > 0) {
+            $berhasil = 0;
+            $gagal = 'Email sudah terpakai, silahkan ganti yang lain';
+        } else {
+            if ($_FILES['gambar']['name'] == "") {
+                // jika tak upload file
+                $gambar = 'user.png';
+            }
+            $query = "insert into user (email,nama_user,password,gambar,hp,tanggal_lahir,alamat,tipe) values ('$email','$nama','$pwd','$gambar','$tlp','$tgl','$alamat','2')";
+            $aw = $db->query($query);
+            if (!$aw) {
+                $gagal = ($db->error . $query);
+                $berhasil = 0;
+            }
+            $berhasil = 1;
+        }
+    } else {
+
+        die('Tak bisa mengkueri database: <br>' . $db->error . $query);
+    }
 }
 
 
@@ -29,49 +55,72 @@ if (isset($_POST['daftar'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="Front end/style/signup.css" />
     <title>Baper!</title>
+
 </head>
 
 <body>
     <main>
         <section class="lupa-password">
-            <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+            <form enctype="multipart/form-data" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
                 <div class="container form-lupa-password">
                     <img class="logo" src="Front end/image/logo.png" alt="logo">
-                    <div class="margin">
+                    <div class="margin" style="font-weight: bolder;">
                         Registrasi Akun
                     </div>
+                    <?php
+                    if (isset($berhasil)) {
+                        if ($berhasil == 1) {
+                            echo '
+                                <div class="alert alert-success alert-dismissible" id="berhasil">
+                                    <h2>Sign Up Berhasil</h2>
+                                    <p>Klik <a class="alert-link" href="./login.php">disini</a> untuk login</p>
+                                    <button class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+                                </div>';
+                        } else {
+                            echo '<div class="alert alert-danger alert-dismissible" id="gagal">
+                                    <h2>Sign Up Gagal</h2>
+                                    <p><? if (isset($gagal)) {
+                                            echo $gagal;
+                                        } ?></p>
+                                    <button class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+                                </div>';
+                        }
+                    }
+                    ?>
                     <div class="margin form-group">
-                        <input class="form-control" type="email" name="email" placeholder="E-Mail">
+                        <input required class="form-control" type="email" name="email" placeholder="E-Mail">
                     </div>
-                    <div class="margin form-group">
+                    <div class=" form-group">
                         <ul class="list-unstyled">
                             <li>
-                                <input class="form-control" type="password" name="pwd1" placeholder="Password">
+                                <input required class="form-control" type="password" name="pwd1" placeholder="Password">
                             </li>
                             <li>
-                                <input class="form-control" type="password" name="pwd2" placeholder="Re-type Password">
+                                <input required class="form-control" type="password" name="pwd2" placeholder="Re-type Password">
                             </li>
-                            <p class="data-diri">
+                            <p class="m-4" style="font-size: larger;font-weight:bold">
                                 Data Diri
                             </p>
+
                             <li>
-                                <input class="form-control" type="text" name="nama" placeholder="Nama Lengkap">
+                                <input required class="form-control" type="text" name="nama" placeholder="Nama Lengkap">
                             </li>
                             <li>
-                                <input class="form-control" type="text" name="tmpt" placeholder="Tempat Lahir">
+                                <input required class="form-control" type="date" name="tgl" placeholder="Tanggal Lahir">
                             </li>
                             <li>
-                                <input class="form-control" type="date" name="tgl" placeholder="Tanggal Lahir">
+                                <input required class="form-control" type="number" name="tlp" placeholder="No Telepon">
                             </li>
                             <li>
-                                <input class="form-control" type="text" name="tlp" placeholder="No Telepon">
+                                <input required class="form-control" type="text" name="alamat" placeholder="Alamat">
                             </li>
-                            <li>
-                                <input class="form-control" type="text" name="alamat" placeholder="Alamat">
+                            <li class="mt-2">
+                                <label for="" class="d-flex justify-start" style="font-family: 'Courier New', Courier, monospace;">Gambar diri</label>
+                                <input class="d-flex justify-start mt-2" type="file" name="gambar">
                             </li>
                         </ul>
                         <div class="daftar">
-                            <button type="submit" name="daftar" class="btn btn-dark">Daftar</button>
+                            <button type="submit" name="daftar" style="width: 100%;" class="btn btn-dark">Daftar</button>
                         </div>
                     </div>
                 </div>
@@ -80,6 +129,7 @@ if (isset($_POST['daftar'])) {
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/5b9f1690ea.js" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
