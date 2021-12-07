fbae18ea38280c0291ea09ff5b5e3f502be03efd
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2021-12-08 02:19:58 +0700

    login

--- login.php
+++ login.php
@@ -1,45 +1,54 @@
 <?php
-  require_once('db_login.php');
-  if (isset($_POST["login"])) {
-      $valid = TRUE;
-      //cek validasi email
-      $email = isset($_POST['email']) ? $_POST['email'] : '';
-      if ($email == '') {
-          $error_email = "email is required";
-          $valid = FALSE;
-      } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          $error_email = "Invalid NIM format";
-          $valid = FALSE;
-      }
-      //cek validasi password
-      $password = test_input($_POST['password']);
-      if ($password == '') {
-          $error_password = "Password is required";
-          $valid = FALSE;
-      }
+session_start();
+require_once('db_login.php');
 
-      //cek validasi
-      if ($valid) {
-          //asign a query
-          $query = "SELECT * FROM user WHERE email='" . $email . "' AND password='" . md5($password) . "' ";
-          //Execute the query
-          $result = $db->query($query);
-          if (!$result) {
-              die("Could not query the database: <br />" . $db->error);
-          } else {
-              if ($result->num_rows > 0) { //login berhasil
-                  $row = $result->fetch_object();
-                  $_SESSION['username'] = $email;
-                  $_SESSION['kategori'] = 'user';
-                  $_SESSION['iduser'] = $row->email;
-                  header('Location:index.php');
-              } else {
-                  $error_password = "Combination username and password are not correct.";
-              }
-          }
-          $db->close();
-      }
+if (isset($_POST["login"])) {
+  $valid = TRUE;
+
+  //cek validasi email
+  $email = isset($_POST['email']) ? $_POST['email'] : '';
+  if ($email == '') {
+    $error_email = "Email is required";
+    $valid = FALSE;
+  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $error_email = "Invalid Email format";
+    $valid = FALSE;
   }
+
+  //cek validasi password
+  $password = test_input($_POST['password']);
+  if ($password == '') {
+    $error_password = "Password is required";
+    $valid = FALSE;
+  }
+
+  //cek validasi
+  if ($valid) {
+    //asign a query
+    $query = "SELECT * FROM user WHERE email='" . $email . "' AND password='" . $password . "' ";
+    //Execute the query
+    $result = $db->query($query);
+    if (!$result) {
+      die("Could not query the database: <br />" . $db->error);
+    } else {
+      if ($result->num_rows > 0) { //login berhasil
+        $row = $result->fetch_object();
+        $_SESSION['username'] = $email;
+        $_SESSION['iduser'] = $row->email;
+        if ($row->tipe == 1) {
+          $_SESSION['kategori'] = 'admin';
+        } else {
+          $_SESSION['kategori'] = 'user';
+        }
+
+        header('Location:index.php');
+      } else {
+        $error_password = "Combination email and password are not correct.";
+      }
+    }
+    $db->close();
+  }
+}
 
 ?>
 
@@ -64,33 +73,35 @@
 <body>
   <main>
     <section class="lupa-password">
-
       <div class="container form-lupa-password">
         <img class="logo" src="Front end/image/logo.png" alt="logo">
         <div class="margin">
           (Balai Perpustakaan)
         </div>
-        <div class="silahkan-masuk">
-          Silahkan Masuk
+        <div class="silakan-masuk">
+          Silakan Masuk
         </div>
         <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" autocomplate="off">
           <div class="margin form-group">
-            <input required class="form-control" type="email" id="email" name="email" placeholder="E-Mail" value="<?php if (isset($email)) {echo $email;}?>">
-
-              <div class="text-danger"><?php if (isset($error_email)) {echo $error_email;} ?></div>
-
+            <input class="form-control" type="text" id="email" name="email" placeholder="Email" value="<?php if (isset($email)) {
+                                                                                                            echo $email;
+                                                                                                          } ?>">
+            <div class="text-danger" style="font-size: small; text-align: left;"><?php if (isset($error_email)) {
+                                        echo $error_email;
+                                      } ?></div>
           </div>
           <div class="margin form-group">
             <ul class="list-unstyled">
               <li>
-                <input required class="form-control" type="password" id="password" name="password" placeholder="Password">
+                <input class="form-control" type="password" id="password" name="password" placeholder="Password">
 
-                <div class="text-danger"><?php if (isset($error_password)) {echo $error_password;} ?></div>
-
+                <div class="text-danger" style="font-size: small; text-align: left;"><?php if (isset($error_password)) {
+                                            echo $error_password;
+                                          } ?></div>
               </li>
             </ul>
             <div class="ganti-password">
-              <button type="submit" name="login" class="btn btn-dark">Login</button>
+              <button type="submit" name="login" class="btn btn-dark" value="submit">Login</button>
             </div>
             <div class="row">
               <div class="col">
@@ -112,4 +123,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/5b9f1690ea.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
