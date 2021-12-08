5a5ce4b215c9e15ac818c6528a4f3713fe5d9107
xontoloyo354 <fahmialqaudy426@Gmail.com>
2021-12-08 16:53:13 +0700

    Buat Footer php new

--- riwayat_peminjaman.php
+++ riwayat_peminjaman.php
@@ -112,58 +112,9 @@
         </div>
     </main>
 
-    <footer class="Footer-menu mt-5 border-top" id="Section-footer">
-        <div class="container" style="background-color: #2c2c2c;">
-            <div class="footers pt-4 pb-4 pt-lg-5 pb-lg-5">
-                <div class="row justify-content-center">
-                    <div class="col-12 pt-3">
-                        <div class="row">
-                            <div class="col-12 col-md-6 col-lg-3 mb-3">
-                                <ul class="list-unstyled footer-link-list">
-                                    <li><img class="mb-2 imglogobrand" src="Front end/image/Group 103.png" alt="imgLogo" /></li>
-                                </ul>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-3 mb-3 gradi">
-                                <h5 class="mb-3 border-bottom pb-2">Information</h5>
-                                <ul class="list-unstyled footer-link-list">
-                                    <li><a class="text-decoration-none" href="#">Home</a></li>
-                                    <li><a class="text-decoration-none" href="#">User</a></li>
-                                    <li><a class="text-decoration-none" href="#">Perpustakaan</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-3 mb-3 get-connected">
-                                <h5 class="mb-3 pb-2 border-bottom">Contact</h5>
-                                <ul class="list-unstyled footer-link-list">
-                                    <li>
-                                        1234 Sample Street <br />
-                                        Austin Texas 78704
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-3 mb-2 get-connected">
-                                <h5 class="mb-3 pb-2 border-bottom">Social Media</h5>
-                                <div class="icon">
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                    <a href="#"><i class="fab fa-linkedin"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="w-100 pt-3 pb-3 copyright border-top" id="footer-copyright">
-            <div class="container text-white" style="background-color: #2c2c2c;">
-                <div class="row pt-2">
-                    <div class="col-12">
-                        <p class="text-center my-1">&copy; 2021 | Perpustakann Baper</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php
+    include('footer.php')
+    ?>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/5b9f1690ea.js" crossorigin="anonymous"></script>
