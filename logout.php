fce20ed2f417b262280105db6f11087b156aac8d
Irvan Muhandis <irvanmuhandis@gmail.com>
2021-12-09 06:47:19 +0700

    nn

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['email'])){
-  unset($_SESSION['email']);
-  unset($_SESSION['username']);
-  unset($_SESSION['kategori']);
-  session_destroy();
-}
-header('Location: index.php');
